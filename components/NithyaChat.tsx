f49849229b45efac4d8fd6b85f39a9fcd495f528
suhas-legacy <suhas@legacyglobalbank.com>
2026-04-21 15:46:26 +0530

    Update API URL to use environment variable

--- components/NithyaChat.tsx
+++ components/NithyaChat.tsx
@@ -118,7 +118,7 @@ export default function NithyaChat() {
 
   const sendContactEmails = async (userData: any) => {
   try {
-    const response = await fetch("http://localhost:3001/api/contact", {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contact`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
